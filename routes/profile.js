24799c3ab39bd500c2f9158888ae3efe1d142ddf
Helene Astaburuaga <127048528+Heleneastaburuaga@users.noreply.github.com>
2023-12-11 14:26:03 +0000

    aldaketak

--- routes/profile.js
+++ routes/profile.js
@@ -9,15 +9,19 @@ const multer  = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/tmp/public/uploads')
+      cb(null, bidea)
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, file.originalname)
     }
   })
   
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024, // 2 MB 
+  }, 
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
