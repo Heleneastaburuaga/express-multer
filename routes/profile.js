ac180a292399c090c2266655d3ef83cec406926b
Helene Astaburuaga <127048528+Heleneastaburuaga@users.noreply.github.com>
2023-12-11 14:02:52 +0000

    aldaketak

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
+
+const bidea= path.join(__dirname, '../public/uploads')
+console.log(bidea);
+
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/public/uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+const upload = multer({ storage: storage })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
